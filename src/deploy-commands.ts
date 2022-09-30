05aa5ce7a8c0dce0e30da059258ce43daabf3d73
Ignacio Baptista [Linux] <ibaptist@redhat.com>
2022-09-30 15:52:20 -0300

    Finished voting logic

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -36,6 +36,44 @@ const commands = [
   new SlashCommandBuilder()
     .setName("test")
     .setDescription("Print all data possible"),
+  new SlashCommandBuilder()
+    .setName("vote-start")
+    .setDescription("Start a new voting session on the current channel")
+    .addNumberOption((option) =>
+      option
+        .setName("total-votes")
+        .setDescription(
+          "Total people participating in the votes, when total is reached, voting is ended automatically"
+        )
+        .setRequired(false)
+    ),
+  new SlashCommandBuilder()
+    .setName("vote")
+    .setDescription("Vote an option")
+    .addStringOption((option) =>
+      option
+        .setName("vote")
+        .setDescription("Your vote")
+        .setRequired(true)
+        .addChoices(
+          { name: "0", value: "0" },
+          { name: "1", value: "1" },
+          { name: "2", value: "2" },
+          { name: "3", value: "3" },
+          { name: "5", value: "5" },
+          { name: "8", value: "8" },
+          { name: "13", value: "13" },
+          { name: "21", value: "21" },
+          { name: "34", value: "34" },
+          { name: "55", value: "55" },
+          { name: "89", value: "89" },
+          { name: "?", value: "?" },
+          { name: "coffee", value: ":coffee:" }
+        )
+    ),
+  new SlashCommandBuilder()
+    .setName("vote-end")
+    .setDescription("End the voting session, and print the results"),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import DiscordJS, { GatewayIntentBits } from "discord.js";
+import DiscordJS, {
+  EmbedBuilder,
+  GatewayIntentBits,
+  TextBasedChannel,
+  User,
+} from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,16 +16,21 @@ const client = new DiscordJS.Client({
   ],
 });
 
-// It is triggered when the bot is ready
+// Its triggered when the bot is ready
 client.on("ready", () => {
   console.log("The bot is ready!");
 });
 
-// It is triggered everytime someone interacts with an interaction
+let voteInProgress = false;
+let voteChannel: TextBasedChannel | null;
+let totalVotes = -1;
+let votes = new Map<User, number>();
+
+// Its triggered when someone interacts with an interaction
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName, options, channelId, user } = interaction;
+  const { commandName, options, channel, user } = interaction;
 
   if (commandName === "ping") {
     await interaction.reply({
@@ -37,17 +47,145 @@ client.on("interactionCreate", async (interaction) => {
     });
   } else if (commandName === "test") {
     await interaction.reply({
-      content: `Channel ID: ${channelId}\n` + `Sender ID: ${user.id}`,
+      content:
+        `**Channel ID:** ${channel?.id} (${channel}) \n` +
+        `**Sender ID:** ${user.id} (${user})`,
       ephemeral: true,
     });
+  } else if (commandName === "vote-start") {
+    if (voteInProgress) {
+      await interaction.reply({
+        content: "There is already a voting in progress",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: `User ${user} started a voting on channel ${channel}, everyone may now vote`,
+        ephemeral: false,
+      });
+      voteInProgress = true;
+      voteChannel = channel;
+
+      if (options.data[0]) {
+        totalVotes = options.data[0].value as number;
+      }
+    }
+  } else if (commandName === "vote-end") {
+    if (!voteInProgress) {
+      await interaction.reply({
+        content: "There is no voting in progress",
+        ephemeral: true,
+      });
+    } else {
+      const voteResultsEmbed = getVoteResults();
+
+      await interaction.reply({
+        embeds: [voteResultsEmbed],
+        ephemeral: false,
+      });
+
+      resetVariables();
+    }
+  } else if (commandName === "vote") {
+    // Check if vote is in progress
+    if (!voteInProgress) {
+      await interaction.reply({
+        content: "There is no vote in progress",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Check if vote is in current channel
+    if (channel !== voteChannel) {
+      await interaction.reply({
+        content: `This is not the current voting channel, go to ${voteChannel} to vote`,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Check if user already voted
+    if (votes.has(user)) {
+      await interaction.reply({
+        content: "You already voted",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Get vote
+    const vote = options.data[0].value as number;
+
+    // Add vote to map
+    votes.set(user, vote);
+
+    await interaction.reply({
+      content: `${user} has voted. **Total votes: ${votes.size}**`,
+      ephemeral: false,
+    });
+
+    // if (totalVotes !== -1 && votes.size === totalVotes) {
+    //   resetVariables();
+    //   const voteResults = getVoteResults();
+    //
+    //   // Send message on channel with vote results
+    //   const channel = await client.channels.fetch(voteChannelID as string);
+    //
+    //   if (channel?.isTextBased()) {
+    //     await channel.send(voteResults);
+    //   }
+    // }
   }
 });
 
-// It is triggered everytime someone sends a message
+// Its triggered when someone sends a message
 client.on("messageCreate", (message) => {
   if (message.content === "ping") {
     void message.reply("pong");
   }
 });
 
-void client.login(process.env.TOKEN);
+client.login(process.env.TOKEN).catch((error) => {
+  console.error("[LOGIN_ERROR] An error has happened: ", error);
+});
+
+const getVoteResults = () => {
+  const exampleEmbed: EmbedBuilder = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Vote results')
+    .setURL('https://discord.js.org/')
+    .setAuthor({ name: 'MinSalBot', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Red_Hat_logo.svg/316px-Red_Hat_logo.svg.png' })
+    //.setDescription('Some description here')
+    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Red_Hat_logo.svg/316px-Red_Hat_logo.svg.png')
+    .addFields(
+      { name: 'Totals', value: 'Some value here' },
+      { name: '\u200B', value: '\u200B' },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+      { name: '\u200b', value: 'Some value here', inline: true },
+    )
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    //.setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Created by nachiten' });
+
+  // Order de votes by number decreasing
+  // const orderedVotes = [...votes.entries()].sort((a, b) => b[1] - a[1]);
+  //
+  // let replyContent = "Voting ended... printing results\n";
+  //
+  // // Reply with the vote results
+  // replyContent += "**Vote results:**\n";
+  // for (const [user, vote] of orderedVotes) {
+  //   replyContent += `User ${user} voted ${vote}\n`;
+  // }
+
+  return exampleEmbed;
+};
+
+const resetVariables = () => {
+  voteInProgress = false;
+  voteChannel = null;
+  totalVotes = -1;
+  votes = new Map<User, number>();
+};
